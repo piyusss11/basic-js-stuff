59b3f6e0da6bb1cb1dc4ebfefc3d0bf691525633
piyusss11 <piyusss11@gmail.com>
2024-04-02 16:22:04 +0530

    gsap

--- color switcher/script.js
+++ color switcher/script.js
@@ -6,19 +6,41 @@ let gray = document.querySelector("#gray");
 let blue = document.querySelector("#blue");
 
 red.addEventListener("click",()=>{
+    gsap.from("#red",{
+        // x:-10,
+        scale:2,
+        duration:1
+    })
     main.style.color ="black";
     main.style.backgroundColor = "lightcoral";
 })
 black.addEventListener("click",()=>{
+
+       gsap.from("#black",{
+           // x:-10,
+           scale:2,
+           duration:1
+       })
+
     main.style.backgroundColor = "black";
     main.style.color="white";
     black.style.border = "1px solid white";
 })
 gray.addEventListener("click",()=>{
+    gsap.from("#gray",{
+        // x:-10,
+        scale:2,
+        duration:1
+    })
     main.style.color ="black";
     main.style.backgroundColor = "lightgray";
 })
 blue.addEventListener("click",()=>{
+    gsap.from("#blue",{
+        // x:-10,
+        scale:2,
+        duration:1
+    })
     main.style.color ="black";
     main.style.backgroundColor = "lightblue";
 })
